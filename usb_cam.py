0529fb72b514bf41d8c80d0156325553b6fe2534
Yasushi Honda <aeroboy08@gmail.com>
2021-06-04 18:16:02 +0900

    record or not

--- usb_cam.py
+++ usb_cam.py
@@ -2,6 +2,8 @@
 import cv2
 import time
 
+record_frame='y'
+
 # open camera
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
 if cap.isOpened():
@@ -14,6 +16,15 @@ if cap.isOpened():
    cap.set(cv2.CAP_PROP_BUFFERSIZE, 4) # BUFFERSIZE を小さくするとレートが速くなる
    cap.set(cv2.CAP_PROP_FPS, 30)
 
+   if record_frame=='y':
+      # 保存用
+      fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+      #frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+      frame_rate = 4
+      width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+      height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+      size = (width, height)
+      vw = cv2.VideoWriter('out.mp4', fmt, frame_rate, size)
 
    count=0
    print("# Input 'q' to stop the camera.")
@@ -25,6 +36,9 @@ if cap.isOpened():
       ret, frame = cap.read()
       cv2.imshow('frame',frame)
       key=cv2.waitKey(1)
+      if record_frame=='y':
+         vw.write(frame)
+        
       count+=1
       now=time.time()
       #time.sleep(0.1)
@@ -36,8 +50,7 @@ if cap.isOpened():
    print("speed=%5.2f (msec)" % speed)
 
 
-   # write frame to file
-   #cv2.imwrite('image.jpg', frame)
+   vw.release()
    # release camera
    cap.release()
 
