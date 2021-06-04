4d1056288263e8ae2bd992f73d1b3e929ffcc617
Yasushi Honda <honda@csse.muroran-it.ac.jp>
2021-06-04 10:28:18 +0900

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import cv2
+import time
+
+# open camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+#cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y', 'U', 'Y', 'V'))
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 4) # BUFFERSIZE を小さくするとレートが速くなる
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+count=0
+key=cv2.waitKey(1)
+now=time.time()
+start=now
+while key!=ord('q'):
+#while now-start<30:
+    # take frame
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+    key=cv2.waitKey(1)
+    count+=1
+    now=time.time()
+    #time.sleep(0.1)
+
+now=time.time()
+rate=count/(now-start)
+speed=1.0/rate*1000
+print("rate=%5.2f (Hz)" % rate)
+print("speed=%5.2f (msec)" % speed)
+
+
+
+# write frame to file
+#cv2.imwrite('image.jpg', frame)
+# release camera
+cap.release()
